dd56e6e12a5b70414de46ce8cab89721b2b68525
Reketov <reshetovandrew@gmail.com>
2016-07-22 16:17:57 +0300

    Создание программы прощания

---
+++ src/ClassWork/data160722/Goodby.java
@@ -0,0 +1,11 @@
+package ClassWork.data160722;
+
+/**
+ * Created by user on 22.07.16.
+ */
+public class Goodby {
+    public static void main(String[] args) {
+        String g = "Goodby";
+        System.out.println(g);
+    }
+}
