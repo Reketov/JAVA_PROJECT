6e958b2f9d7a6e2dc85ddbf8d0800601bae794c7
Sagayda4niyAlexey <Sagayda4niyAlexey@ukr.netl>
2016-07-22 14:36:05 +0300

    Мой первый Commit

---
+++ src/ClassWork/data160722/Test.java
@@ -0,0 +1,16 @@
+package ClassWork.data160722;
+
+/**
+ * Created by user on 22.07.16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        String s = "Test";
+        String q = "Test1";
+        int random = (int) (Math.random() * 2);
+        if (random == 1) {
+            System.out.println(s);
+            System.out.println(q);
+        }
+    }
+}
