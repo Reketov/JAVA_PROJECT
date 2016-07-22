42505b056ca1613714bd8d7db2210a8828bcf654
Reketov <reshetovandrew@gmail.com>
2016-07-22 14:46:06 +0300

    Добавлен символ

--- src/ClassWork/data160722/Test.java
+++ src/ClassWork/data160722/Test.java
@@ -6,7 +6,7 @@ package ClassWork.data160722;
 public class Test {
     public static void main(String[] args) {
         String s = "Test";
-        String q = "Test1";
+        String q = "Test2";
         int random = (int) (Math.random() * 2);
         if (random == 1) {
             System.out.println(s);
